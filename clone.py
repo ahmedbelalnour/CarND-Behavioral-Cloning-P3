f53c74227872b64a57d21ae1c5bd877b0c2e2a1c
Ahmed Belal <carnd@ip-172-31-4-2.us-west-1.compute.internal>
2018-07-07 10:59:12 +0000

    replacing linux '/' fowrard slash with '\' windows back slash for path string

--- clone.py
+++ clone.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 lines = []
-with open(../simulation_training_data/driving_log.csv) as csvfile:
+with open('./simulation_training_data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
 		lines.append(row)
@@ -12,9 +12,10 @@ images = []
 measurments = []
 for line in lines:
 	source_path = line[0]
-	filename = source_path.split('/')[-1]
-	current_path = '../simulation_training_data/IMG/' + filename
+	filename = source_path.split('\\')[-1]
+	current_path = './simulation_training_data/IMG/' + filename
 	image = cv2.imread(current_path)
+	print(current_path)
 	images.append(image)
 	measurment = float(line[3])
 	measurments.append(measurment)
@@ -23,15 +24,16 @@ x_train = np.array(images)
 y_train = np.array(measurments)	
 
 
-from keras.models import Sequesntial
+from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
-model = Sequesntial()
+
+model = Sequential()
 model.add(Flatten(input_shape = (160,320,3))) 
 model.add(Dense(1))
 
 model.compile(loss = 'mse', optimizer = 'adam')
-model.fit(x_train, y_train, validation_split = 0.2, shuffle = True, np_epoch = 7)
+model.fit(x_train, y_train, validation_split = 0.2, shuffle = True, nb_epoch = 7)
 
 model.save('model.h5')
 
