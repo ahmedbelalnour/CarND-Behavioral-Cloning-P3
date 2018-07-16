8437b2753ccdf4badc29ca6dcd0b080c26dc581e
Ahmed Belal <ahmedbelalnour@gmail.com>
2018-07-16 10:06:10 +0200

    - Removing print statements.

--- clone.py
+++ clone.py
@@ -124,14 +124,15 @@ model.save('model.h5')
 
 import matplotlib.pyplot as plt
 
+
 ### print the keys contained in the history object
-print(history_object.history.keys())
+# print(history_object.history.keys())
 
 ### plot the training and validation loss for each epoch
-plt.plot(history_object.history['loss'])
-plt.plot(history_object.history['val_loss'])
-plt.title('model mean squared error loss')
-plt.ylabel('mean squared error loss')
-plt.xlabel('epoch')
-plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
+# plt.plot(history_object.history['loss'])
+# plt.plot(history_object.history['val_loss'])
+#plt.title('model mean squared error loss')
+#plt.ylabel('mean squared error loss')
+#plt.xlabel('epoch')
+#plt.legend(['training set', 'validation set'], loc='upper right')
+#plt.show()
